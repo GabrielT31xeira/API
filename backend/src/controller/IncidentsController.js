9d3ccd4f9a0fd04c707ee8c8737e0291a24eace3
Teixeira <gt3ixeira@gmail.com>
2020-12-21 11:12:18 -0300

    finsh controllers

--- backend/src/controller/IncidentsController.js
+++ backend/src/controller/IncidentsController.js
@@ -1,9 +1,26 @@
 const connection = require('../database/connection');
 const { index } = require('./OngController');
 
+
 module.exports = {
     async index(request,response){
-        const incidents = await connection('incidents').select('*');
+        const { page = 1 } = request.query;
+
+        const count = await connection('incidents').count();
+
+        const incidents = await connection('incidents')
+        .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+        .limit(5)
+        .offset((page - 1)*5)
+        .select(['incidents.*',
+        'ongs.name',
+        'ongs.email',
+        'ongs.whatsapp',
+        'ongs.cidade',
+        'ongs.uf'
+    ]);
+
+        response.header('X-Total-Count', count['count(*)']);
 
         return response.json(incidents);
     },
@@ -19,5 +36,22 @@ module.exports = {
             ong_id,
         });
         return response.json({ id });
+    },
+
+    async delete (request, response){
+        const { id } = request.params; 
+        const ong_id = request.headers.authorization;
+
+        const incident = await connection('incidents')
+        .where('id', id)
+        .select('ong_id')
+        .first();
+
+        if(incident.ong_id !== ong_id){
+            return response.status(401).json({ error: 'Voçê não tem permição para fazer isso...'});
+        }
+            await connection('incidents').where('id', id).delete();
+            return response.status(204).send();     
+        
     }
 };
