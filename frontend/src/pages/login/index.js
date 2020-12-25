c7097c66118e72854a0451962711112a98445cc7
Teixeira <gt3ixeira@gmail.com>
2020-12-25 13:05:25 -0300

    Update frontend

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi'
 
 import './style.css';
 
 export default function Login() {
-    return(
-        <h1>Login</h1>
+    return (
+        <div className="logon-container">
+            <section className="form">
+                <form>
+                    <h1>Faça seu login</h1>
+                    <input placeholder="Sua ID" />
+                    <button className="buttom" type="submit">Entrar</button>
+                    <a href="/register">
+                        <FiLogIn size={16} />
+                    Não tenho cadastro</a>
+                </form>
+            </section>
+        </div>
     );
 }
